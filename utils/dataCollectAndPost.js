a6f43d5f97f1142782ff2215b22140198c67c4a3
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-09-03 23:05:20 +0600

    file name change

--- utils/dataCollectAndPost.js
+++ utils/dataCollectAndPost.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 // console.log(data)
 
 module.exports.getOldData = () => {
-    const data = JSON.parse(fs.readFileSync('data.json'))
+    const data = JSON.parse(fs.readFileSync('config.json'))
     return data
 }
 
 module.exports.postAData = (data) => {
     const dataStringify = JSON.stringify(data)
-    return fs.writeFileSync('data.json', dataStringify)
+    return fs.writeFileSync('config.json', dataStringify)
 }
 
